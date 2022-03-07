f7308b3c06db15bc84e3c1043846a982b9d070ea
bjorger <robin.braumann@gmail.com>
2022-03-07 15:33:03 +0100

    update react-app-rewired

---
+++ src/components/contact/contact.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { PageWrap } from "components/page";
+import styled from "styled-components";
+
+const ContactUs: React.FC = () => {
+    return <PageWrap variant="white"></PageWrap>;
+};
+
+export default ContactUs;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,8 +1,13 @@
 import { PageContainer } from "components/page";
 import React from "react";
+import ContactUs from "components/contact/contact";
 
 const Contact: React.FC = () => {
-    return <PageContainer eyebrow="Wir sind für sie da" title="Kontakt"></PageContainer>;
+    return (
+        <PageContainer eyebrow="Wir sind für sie da" title="Kontakt">
+            <ContactUs />
+        </PageContainer>
+    );
 };
 
 export default Contact;
