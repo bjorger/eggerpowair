1437586a778c586437529b1be2922c549dff5d33
bjorger <robin.braumann@gmail.com>
2022-03-07 15:35:12 +0100

    start with contact page

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -3,7 +3,13 @@ import { PageWrap } from "components/page";
 import styled from "styled-components";
 
 const ContactUs: React.FC = () => {
-    return <PageWrap variant="white"></PageWrap>;
+    return (
+        <PageWrap variant="white">
+            <ContactWrapper></ContactWrapper>
+        </PageWrap>
+    );
 };
 
 export default ContactUs;
+
+const ContactWrapper = styled.div``;
