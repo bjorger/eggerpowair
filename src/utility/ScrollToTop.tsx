4a575477c0522e14b8f4e5850e4c3009297f8418
Robin Braumann <robin.braumann@gmail.com>
2022-03-24 17:24:03 +0100

    remove unused library

--- src/utility/ScrollToTop.tsx
+++ src/utility/ScrollToTop.tsx
@@ -1,10 +1,8 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router";
-import { useSearchParams } from "react-router-dom";
 
 const ScrollToTop: React.FC = ({ children }) => {
     const location = useLocation();
-    const [searchParams, setSearchParams] = useSearchParams();
 
     useEffect(() => {
         console.log(window.location);
